f4491d270e3492c8c4a830fc39ba4630c8325713
Rachel Bittner <Rachel@Santa.local>
2014-09-10 17:02:54 +0200

    added assertions

--- multitrack/__init__.py
+++ multitrack/__init__.py
@@ -9,6 +9,13 @@ config_vars = yaml.load(f_in)
 f_in.close()
 
 MEDLEYDB_DIR = config_vars['MEDLEYDB_DIR']
+assert MEDLEYDB_DIR, """No local directory set in config.yaml 
+Set the value of MEDLEYDB_DIR in medleydb/multitrack/config.yaml 
+to your local path to the MedleyDB dataset."""
+
+assert path.exists(MEDLEYDB_DIR), """The absolute path:
+%s
+set in config.yaml does not exist.""" % MEDLEYDB_DIR
 
 INST_TAXONOMY = path.join(path.dirname(__file__), 'taxonomy.yaml')
 AUDIO_DIR = path.join(MEDLEYDB_DIR, 'Audio')
