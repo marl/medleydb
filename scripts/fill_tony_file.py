338a4ca8f3ed5af0334ce4bb6dc540756cd999dd
Rachel Bittner <rabitt3@users.noreply.github.com>
2014-09-16 18:24:44 +0200

    code bettering

--- scripts/fill_tony_file.py
+++ scripts/fill_tony_file.py
@@ -26,96 +26,144 @@ HOP = 256.0 #Tony default hop size
 FS = 44100.0 #Tony default output sample rate
 
 
-def fill_f0_track(tony_csv_file, output_file_path, total_duration):
-    """ Fill in missing time frames in a Tony file, write the output to file.
-    Params:
-        - tony_csv_file: path to tony ouputted csv file
-        - output_file_path: path to save location of filled in file
-        - total_duration: length (in seconds) of the annotation audiofile
-    """
-    tony_f0 = read_tony_file(tony_csv_file)
-
-    if not tony_f0:
-        return False
-
-    if tony_f0:
-        start_idx = sec_to_idx(0)
-        end_idx = sec_to_idx(total_duration)
-        f0_sequence = make_blank_f0_sequence(total_duration)
-        for time_freq in tony_f0:
-            time = time_freq[0]
-            freq = time_freq[1]
-            time_idx = sec_to_idx(time)
-            if time_idx >= start_idx and time_idx < end_idx:
-                f0_sequence[time_idx][1] = freq
-    
-    result = write_f0_to_csv(f0_sequence, output_file_path)
-    if result:
-        return True
-    else:
-        return False
-
-
 def read_tony_file(fpath):
-    """ Read a Tony csv file.
+    """Read a tony-generated csv file.
+
+    Parameters
+    ----------
+    fpath : str
+        Path to tony-generated csv file.
+
+    Returns
+    -------
+    tony_f0 : array
+        Array of tony-generated f0 values and corresponding time stamps.
     """
-    if os.path.exists(fpath):
-        data_lines = [line.strip() for line in open(fpath)]
-        tony_f0 = []
-        for line in data_lines:
-            time, f_0 = line.split(',')[0:2]
-            tony_f0.append([float(time), float(f_0)])
-        return tony_f0
-    else:
-        return False
+    assert os.path.exists(fpath), "File path does not exist."
+
+    data_lines = [line.strip() for line in open(fpath)]
+    tony_f0 = []
+    for line in data_lines:
+        time, f_0 = line.split(',')[0:2]
+        tony_f0.append([float(time), float(f_0)])
+    return tony_f0
 
 
 def get_time_stamps(total_duration):
-    """ Get a list of evenly spaced time stamps based on Tony's parameters.
-    Params:
-        - total_duration: length (in seconds) of the annotation audiofile
+    """Get a list of evenly spaced time stamps based on Tony's parameters.
+
+    Parameters
+    ----------
+    total_duration : float
+        Length (in seconds) of annotation's corresponding audio file.
+
+    Returns
+    -------
+    time_stamps : array
+        Array of evenly spaced time stamps spanning the entire annotation.
     """
-    time_stamps = []
     n_stamps = int(np.ceil((total_duration*FS)/HOP))
-    time_stamps = np.array(range(n_stamps))*(HOP/FS)
-    return time_stamps
+    return np.array(range(n_stamps))*(HOP/FS)
 
 
 def make_blank_f0_sequence(total_duration):
-    """ Make a complete f0 sequence filled with 0's.
-        First column is time, second column is frequency.
+    """Make a complete f0 sequence filled with 0's.
+    First column is time, second column is frequency.
+
+    Parameters
+    ----------
+    total_duration : float
+        Length (in seconds) of annotation's corresponding audio file.
+
+    Returns
+    -------
+    f0_sequence : array
+        Array of filled in f0 values and corresponding time stamps.
     """
     time_stamps = get_time_stamps(total_duration)
     f0_sequence = np.zeros((len(time_stamps), 2))
-    for i, time_stamp in enumerate(time_stamps):
-        f0_sequence[i][0] = time_stamp
+    f0_sequence[:, 0] = time_stamps
     return f0_sequence
 
 
-def sec_to_idx(time_in_seconds, fs=FS, hop=HOP):
-    """ Get the index of a given time stamps
-    """
-    return int(np.round(time_in_seconds*fs/hop))
+def sec_to_idx(time_in_seconds, sample_rate=FS, hop=HOP):
+    """Compute the array index of a given time stamp.
+
+    Parameters
+    ----------
+    time_in_seconds : float
+        Time stamp, in seconds.
+    sample_rate : float
+        Annotation sample rate.
+    hop : float
+        Annotation hop size.
+
+    Returns
+    -------
+    array_idx : int
+        Index of time stamp in filled f0 array.
+    """ 
+    return int(np.round(time_in_seconds*sample_rate/hop))
 
 
 def write_f0_to_csv(f0_sequence, output_file_path):
-    """ Write f0 sequence to a csv file.
+    """Write f0 sequence to a csv file.
+
+    Parameters
+    ----------
+    f0_sequence : array
+        Filled f0 sequence with corresponding time stamps.
+    output_file_path : str
+        Path to save csv file.
+    """     
+    assert len(f0_sequence) != 0, "f0 sequence is empty."
+    with open(output_file_path, "wb") as fpath:
+        writer = csv.writer(fpath)
+        writer.writerows(f0_sequence)
+
+
+def main(args):
+    """Fill in missing time frames in a Tony file, write the output to file.
+
+    Parameters
+    ----------
+    tony_csv_file : str
+        Path to tony-generated csv file.
+    output_file_path : str
+        Path to save location of filled-in file.
+    total_duration : float
+        Length (in seconds) of the annotation's corresponding audio file.
     """
-    if len(f0_sequence):
-        with open(output_file_path, "wb") as f:
-            writer = csv.writer(f)
-            writer.writerows(f0_sequence)
-        return True
-    else:
-        return False
+
+    tony_f0 = read_tony_file(args.tonyfile)
+
+    assert tony_f0, "Tony file does not exist."
+
+    start_idx = sec_to_idx(0)
+    end_idx = sec_to_idx(args.total_duration)
+    f0_sequence = make_blank_f0_sequence(args.total_duration)
+    for time_freq in tony_f0:
+        time = time_freq[0]
+        freq = time_freq[1]
+        time_idx = sec_to_idx(time)
+        if time_idx >= start_idx and time_idx < end_idx:
+            f0_sequence[time_idx][1] = freq
+    
+    write_f0_to_csv(f0_sequence, args.outputfile)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Fill in missing time frames in a Tony file, write the output to file.")
-    parser.add_argument("tonyfile", help="path to tony ouput csv file")
-    parser.add_argument("outputfile", help="path to save location of filled in file")
-    parser.add_argument("duration", help="length (in seconds) of the annotation audiofile")
+    parser = argparse.ArgumentParser(
+        description="Fill in missing time frames in a Tony file," 
+                    "write the output to file.")
+    parser.add_argument("tonyfile", 
+                        type=str, 
+                        help="Path to tony ouput csv file.")
+    parser.add_argument("outputfile", 
+                        type=str, 
+                        help="Path to save location of filled in file.")
+    parser.add_argument("duration", 
+                        type=float, 
+                        help="Length (in seconds) of annotation audio file.")
 
-    args = parser.parse_args()
-
-    fill_f0_track(args.tonyfile, args.outputfile, float(args.duration))
+    main(parser.parse_args())
