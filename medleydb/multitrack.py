4333788badeb78596ed247781ee8f73df1c157ca
Rachel Bittner <rabitt3@users.noreply.github.com>
2014-10-21 18:47:50 +0200

    all the docstrings

--- medleydb/multitrack.py
+++ medleydb/multitrack.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Class definitions for MedleyDB multitracks.
-"""
+"""Class definitions for MedleyDB multitracks."""
 
 import os
 import yaml
@@ -29,26 +28,60 @@ _PITCH_FMT = "%s.csv"
 
 
 class MultiTrack(object):
-    """ MultiTrack Class definition.
+    """MultiTrack Class definition.
+
+    This class loads all available metadata, annotations, and filepaths for a
+    given multitrack directory.
+
+    Examples:
+        >>> mtrack = Multitrack('/MedleyDB/Audio/LizNelson_Rainfall')
+        >>> another_mtrack = Multitrack('some/path/ArtistName_TrackTitle')
+
+    Attributes:
+        artist (str): Artist.
+        duration (float): Track duration, in seconds.
+        genre (str): Track genre label.
+        has_bleed (bool): True if track has stems with bleed.
+        is_excerpt (bool): True if track is an excerpt.
+        is_instrumental (bool): True if track is instrumental.
+        melody1_annotation (list): time, f0 lists from melody 1 annotation.
+        melody2_annotation (list): time, f0 lists from melody 2 annotation.
+        melody3_annotation (list): time, f0 lists from melody 3 annotation.
+        mix_path (str): Full path to MIX file.
+        mtrack_path (str): Full path to folder containing multitrack.
+        origin (str): Track origin.
+        raw_audio (list of Track objects): List of raw audio tracks.
+        raw_instruments (list of strings): List of raw track instrument labels.
+        stem_instruments (list of strings): List of stem instrument labels.
+        stems (list of Track objects): List of stems.
+        title (str): Track title.
+        track_id (str): Unique track id in the form "ArtistName_TrackTitle".
+
     """
 
-    def __init__(self, mtrack_dir):
+    def __init__(self, mtrack_path):
+        """MultiTrack object __init__ method.
+
+        Args:
+            mtrack_path (str): Path to folder with multitrack information.
+
+        """
 
         # Artist, Title & Track Directory #
-        self.mtrack_dir = mtrack_dir
-        self.artist = _path_basedir(mtrack_dir).split('_')[0]
-        self.title = _path_basedir(mtrack_dir).split('_')[1]
+        self.mtrack_path = mtrack_path
+        self.artist = _path_basedir(mtrack_path).split('_')[0]
+        self.title = _path_basedir(mtrack_path).split('_')[1]
         self.track_id = _TRACKID_FMT % (self.artist, self.title)
 
         # Filenames and Filepaths #
         self._meta_basename = _METADATA_FMT % self.track_id
-        self._meta_path = os.path.join(mtrack_dir, self._meta_basename)
+        self._meta_path = os.path.join(mtrack_path, self._meta_basename)
         self._stem_dir_basename = _STEMDIR_FMT % self.track_id
-        self._stem_dir_path = os.path.join(mtrack_dir, self._stem_dir_basename)
+        self._stem_dir_path = os.path.join(mtrack_path, self._stem_dir_basename)
         self._raw_dir_basename = _RAWDIR_FMT % self.track_id
-        self._raw_dir_path = os.path.join(mtrack_dir, self._raw_dir_basename)
+        self._raw_dir_path = os.path.join(mtrack_path, self._raw_dir_basename)
         self._mix_basename = _MIX_FMT % self.track_id
-        self.mix_path = os.path.join(mtrack_dir, self._mix_basename)
+        self.mix_path = os.path.join(mtrack_path, self._mix_basename)
 
         # Stem & Raw File Formats #
         self._stem_fmt = _STEM_FMT % self.track_id
@@ -79,14 +112,14 @@ class MultiTrack(object):
         self._fill_melody_annotations()
 
     def _load_metadata(self):
-        """ Load the metadata file.
+        """Load the metadata file. 
         """
         with open(self._meta_path, 'r') as f_in:
             metadata = yaml.load(f_in)
         return metadata
 
     def _parse_metadata(self):
-        """ Parse metadata dictionary into useful things.
+        """Parse metadata dictionary. 
         """
         stems = []
         raw_audio = []
@@ -99,7 +132,7 @@ class MultiTrack(object):
             file_path = os.path.join(self._stem_dir_path, file_name)
 
             track = Track(instrument=instrument, file_path=file_path,
-                          component=component, stem_idx=k,
+                          component=component, stem_idx=k[1:],
                           mix_path=self.mix_path)
 
             stems.append(track)
@@ -118,7 +151,7 @@ class MultiTrack(object):
         return stems, raw_audio
 
     def _fill_melody_annotations(self):
-        """ Fill melody annotations if files exists
+        """Fill melody annotations if files exists. 
         """
         melody1_fname = _MELODY1_FMT % self.track_id
         melody2_fname = _MELODY2_FMT % self.track_id
@@ -133,43 +166,108 @@ class MultiTrack(object):
         self.melody3_annotation = read_annotation_file(melody3_fpath)
 
     def melody_tracks(self):
-        """ Get list of tracks that contain melody """
+        """Get list of tracks that contain melody. 
+
+        Returns:
+            List of track objects where component='melody'. 
+
+        """
         return [track for track in self.stems if track.component == 'melody']
 
     def bass_tracks(self):
-        """ Get list of tracks that contain bass """
+        """Get list of tracks that contain bass. 
+
+        Returns:
+            List of track objects where component='bass'. 
+          
+        """
         return [track for track in self.stems if track.component == 'bass']
 
     def num_stems(self):
-        """ Number of stems. """
+        """Number of stems. 
+
+        Returns:
+            Number of stems (as an int). 
+          
+        """
         return len(self.stems)
 
     def num_raw(self):
-        """ Number of raw audio files. """
+        """Number of raw audio files. 
+
+        Returns:
+            Number of raw audio files (as an int). 
+          
+        """
         return len(self.raw_audio)
 
     def stem_filepaths(self):
-        """ Get list of filepaths to raw audio files. """
+        """Get list of filepaths to stem files.  
+
+        Returns:
+            List of filepaths to stems. 
+          
+        """
         return [track.file_path for track in self.stems]
 
     def raw_filepaths(self):
-        """ Get list of filepaths to raw audio files. """
+        """Get list of filepaths to raw audio files.  
+
+        Returns:
+            List of filepaths to raw audio files.
+          
+        """
         return [track.file_path for track in self.raw_audio]
 
     def raw_from_stem(self, stem_idx):
-        """ Get all raw audio tracks for a given stem index. """
+        """Get all raw audio tracks that are children of a given stem.  
+
+        Args:
+            stem_idx (int): stem index (eg. 2 for stem S02)
+
+        Returns:
+            List of Track objects. 
+          
+        """
         return [track for track in self.raw_audio if track.stem_idx == stem_idx]
 
-class Track(MultiTrack):
-    """ Track class definition.
+
+class Track(object):
+    """Track class definition.
+
+    Used for stems and for raw audio tracks.
+
+    Attributes:
+        component (str): Description of `attr3`.
+        duration (float): Length of corresponding audio file (in seconds).
+        file_path (str): Path to corresponding audio file.
+        instrument (str): Instrument label.
+        mix_path (str): Path to tracks corresponding mix file.
+        pitch_annotation (list): List of time, f0 values.
+        raw_idx (int): Index of corresponding raw audio file (None if a stem)
+        stem_idx (int): Index of corresponding stem file.
+
     """
+
     def __init__(self, instrument='', file_path='', component='',
-                 stem_idx=None, raw_idx=None, mix_path =''):
+                 stem_idx=None, raw_idx=None, mix_path=''):
+        """Track object __init__ method.
+
+        Args:
+            instrument (str): the track's instrument label.
+            file_path (str): path to corresponding audio file.
+            component (str): stem's component label, if exists.
+            stem_idx (int or str): stem index, either as integer or string
+                For ArtistName_TrackTitle_STEM_05.wav, either 5 or 'S05'
+            raw_idx (int or str): raw index, either as integer or string
+                For ArtistName_TrackTitle_RAW_05_02.wav, either 2 or 'R02'
+            mix_path (str): path to corresponding mix audio file.
+        """
         self.instrument = instrument
         self.file_path = file_path
         self.component = component
-        self.stem_idx = stem_idx
-        self.raw_idx = raw_idx
+        self.stem_idx = self._format_index(stem_idx)
+        self.raw_idx = self._format_index(raw_idx)
         self.duration = get_duration(file_path)
         self.mix_path = mix_path
         self.pitch_annotation = None
@@ -177,17 +275,27 @@ class Track(MultiTrack):
         if self.component == 'melody':
             self._fill_pitch_annotation()
 
+    def _format_index(self, index):
+        """Load stem or raw index. Reformat if in string form. 
+        """
+        if isinstance(index, str):
+            return int(index.strip('S').strip('R'))
+        elif index is None:
+            return None
+        else:
+            return int(index)
+
     def _fill_pitch_annotation(self):
-        """ Fill pitch annotation if file exists.
+        """Fill pitch annotation if file exists. 
         """
         fname = _PITCH_FMT % os.path.basename(self.file_path).split('.')[0]
         pitch_annotation_fpath = os.path.join(PITCH_DIR, fname)
         self.pitch_annotation = read_annotation_file(pitch_annotation_fpath, 
-                                                     maxcols=2)
+                                                     num_cols=2)
 
 
 def _path_basedir(path):
-    """ Get the name of the lowest directory of a path
+    """Get the name of the lowest directory of a path. 
     """
     norm_path = os.path.normpath(path)
     return os.path.basename(norm_path)
@@ -196,15 +304,12 @@ def _path_basedir(path):
 def _get_dict_leaves(dictionary):
     """Get the set of all leaves of a dictionary.
 
-    Parameters
-    ----------
-    dictionary : dict
-        Any dictionary.
+    Args:
+        dictionary (dict): Any dictionary.
 
-    Returns
-    -------
-    vals : set
-        Set of leaf values.
+    Returns:
+        vals (set): Set of leaf values.
+
     """
     vals = []
     if type(dictionary) == dict:
@@ -224,36 +329,61 @@ def _get_dict_leaves(dictionary):
     return vals
 
 
-def get_duration(fname):
-    """Return the file duration, in seconds.
+def get_duration(wave_fpath):
+    """Return the duration of a wave file, in seconds.
 
-    Parameters
-    ----------
-    fname : str
-        Audio file.
+    Example:
+        >>> get_duration('my_favorite_song.wav')
+        17.46267573696145
 
-    Returns
-    -------
-    duration : float
-        Duration of audio file in seconds.
+    Args:
+        wave_fpath (str): Wave file.
+
+    Returns:
+        duration (float): Duration of wave file in seconds.
+
     """
-    fpath = wave.open(fname, 'rb')
+    fpath = wave.open(wave_fpath, 'rb')
     nsamples = fpath.getnframes()
     sample_rate = fpath.getframerate()
     fpath.close()
     return float(nsamples)/float(sample_rate)
 
 
-def read_annotation_file(fpath, maxcols=None):
-    """ Read an annotation file.
+def read_annotation_file(fpath, num_cols=None):
+    """Read an annotation file.
+
+    Examples:
+        >>> melody_fpath = 'ArtistName_TrackTitle_MELODY1.txt'
+        >>> pitch_fpath = 'my_tony_pitch_annotation.csv'
+        >>> melody_annotation = read_annotation_file(melody_fpath)
+        >>> pitch_annotation = read_annotation_file(pitch_fpath, num_cols=2)
+
+        The returned annotations can be directly converted to a numpy array,
+            if desired.
+
+    Note:
+        When reading files generated by Tony, set num_cols=2.
+        Annotation files created by Tony can contain a third column that
+        sometimes has a value (e.g [2]) and sometimes does not. It isn't
+        important for annotation and can be ignored.
+
+    Args:
+        fpath (str): Path to annotation file.
+        num_cols (int): Number of columns to read. If specified, will only read
+            the return num_cols columns of the annotation file.
+
+    Returns:
+        annotation (list): List of rows of the annotation file.
+
     """
     if os.path.exists(fpath):
         with open(fpath) as f_handle:
             annotation = []
             linereader = csv.reader(f_handle)
             for line in linereader:
-                if maxcols:
-                    line = line[:maxcols]
+                if num_cols:
+                    line = line[:num_cols]
                 annotation.append([float(val) for val in line])
         return annotation
     else:
@@ -263,33 +393,40 @@ def read_annotation_file(fpath, maxcols=None):
 def get_valid_instrument_labels(taxonomy_file=INST_TAXONOMY):
     """Get set of valid instrument labels based on a taxonomy.
 
-    Parameters
-    ----------
-    taxonomy_file : str
-        Path to instrument taxonomy file.
+    Examples:
+        >>> valid_labels = get_valid_instrument_labels()
+        >>> my_valid_labels = get_valid_instrument_labels('my_taxonomy.yaml')
 
-    Returns
-    -------
-    instrument_list : set
-        Set of valid instrument labels.
+    Args:
+        taxonomy_file (str): Path to instrument taxonomy file.
+
+    Returns:
+        valid_instrument_labels (set): Set of valid instrument labels.
+
     """
     with open(taxonomy_file) as f_handle:
         taxonomy = yaml.load(f_handle)
-    instrument_list = _get_dict_leaves(taxonomy)
-    return instrument_list
+    valid_instrument_labels = _get_dict_leaves(taxonomy)
+    return valid_instrument_labels
 
 
 def is_valid_instrument(instrument):
     """Test if an instrument is valid based on a taxonomy.
+        This is case sensitive! Taxonomy instrument labels are all lowercase.
 
-    Parameters
-    ----------
-    instrument : str
-        Input instrument.
+    Examples:
+        >>> is_valid_instrument('clarinet')
+        True
+        >>> is_valid_instrument('Clarinet')
+        False
+        >>> is_valid_instrument('mayonnaise')
+        False
 
-    Returns
-    -------
-    value: bool
-        True if instrument is valid.
+    Args:
+        instrument (str): Input instrument.
+
+    Returns:
+        value (bool): True if instrument is valid.
+
     """
     return instrument in get_valid_instrument_labels()
