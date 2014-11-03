628455cc42f167bb27ac4d15ad8750bb748786f9
Rachel Bittner <rachel.bittner@nyu.edu>
2014-11-03 16:41:46 +0100

    fixed #3

--- medleydb/__init__.py
+++ medleydb/__init__.py
@@ -9,16 +9,16 @@ from os import environ
 logging.basicConfig(level=logging.CRITICAL)
 
 assert "MEDLEYDB_PATH" in environ, """The environment variable MEDLEYDB_PATH 
-is not set. Set the value of MEDLEYDB_DIR to your local path to MedeleyDB."""
+is not set. Set the value of MEDLEYDB_PATH to your local path to MedeleyDB."""
 
-MEDLEYDB_DIR = environ["MEDLEYDB_PATH"]
+MEDLEYDB_PATH = environ["MEDLEYDB_PATH"]
 
-assert path.exists(MEDLEYDB_DIR), """The path: %s set for MEDLEYDB_PATH does 
-not exist.""" % MEDLEYDB_DIR
+assert path.exists(MEDLEYDB_PATH), """The path: %s set for MEDLEYDB_PATH does 
+not exist.""" % MEDLEYDB_PATH
 
 INST_TAXONOMY = path.join(path.dirname(__file__), 'taxonomy.yaml')
-AUDIO_DIR = path.join(MEDLEYDB_DIR, 'Audio')
-ANNOTATION_DIR = path.join(MEDLEYDB_DIR, 'Annotations')
+AUDIO_DIR = path.join(MEDLEYDB_PATH, 'Audio')
+ANNOTATION_DIR = path.join(MEDLEYDB_PATH, 'Annotations')
 MELODY_DIR = path.join(ANNOTATION_DIR, 'Melody_Annotations')
 PITCH_DIR = path.join(ANNOTATION_DIR, 'Pitch_Annotations')
 
