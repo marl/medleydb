d7519c7dc190e0591b2b2756d180bbbb67288315
Rachel Bittner <rachel.bittner@nyu.edu>
2015-04-20 22:24:31 -0400

    update setup to include medleyalchemy

--- setup.py
+++ setup.py
@@ -2,30 +2,62 @@ from setuptools import setup
 
 setup(
     name='medleydb',
+
     version='0.1.0',
+
     description='Python module for the MedleyDB dataset',
+
     author='Rachel Bittner',
+
     author_email='rachel.bittner@nyu.edu',
+
     url='https://github.com/rabitt3/medleydb',
+
     download_url='http://github.com/rabitt3/medleydb/releases',
-    packages=['medleydb'],
+
+    packages=['medleydb', 'medleyalchemy'],
+
     package_data={'medleydb': ['taxonomy.yaml']},
+
     long_description="""A python module for audio and music processing.""",
+
     classifiers=[
         "License :: The MIT License (MIT)",
         "Programming Language :: Python",
         "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Topic :: Multimedia :: Sound/Audio :: Analysis"
+        'Intended Audience :: Telecommunications Industry',
+        'Intended Audience :: Science/Research',
+        'Environment :: Console',
+        'Environment :: Plugins',
+        'Topic :: Multimedia :: Sound/Audio :: Analysis',
+        'Topic :: Multimedia :: Sound/Audio :: Sound Synthesis'
+        'Topic :: Scientific/Engineering :: Information Analysis',
     ],
+
     keywords='dataset multitrack music',
+
     license='MIT',
+
     install_requires=['pyyaml'],
+
     extras_require={
         'tests': [
             'pytest',
             'pytest-cov',
             'pytest-pep8',
         ],
+        'sql': [
+            'SQLAlchemy'
+        ],
+        'docs': [
+            'sphinx',
+            'sphinxcontrib-napoleon',
+            'sphinx_rtd_theme',
+            'numpydoc',
+        ],
     },
+
+    entry_points={'console_scripts': [
+        'medleydb-export=medleyalchemy:export'
+    ]}
 )
