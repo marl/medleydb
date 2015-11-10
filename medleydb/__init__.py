b223058dc52bf52e0e59d1a03ccde991ea195f01
Rachel Bittner <rachel.bittner@nyu.edu>
2015-11-10 18:04:04 -0500

    update paths, reduce dependency on MEDLEYDB_PATH

--- medleydb/__init__.py
+++ medleydb/__init__.py
@@ -3,23 +3,31 @@
 """ Python tools for using MedleyDB """
 
 import logging
-from version import __version__
 from os import path
 from os import environ
+
+from medleydb.version import __version__
 
 __all__ = ["__version__", "sql"]
 
 logging.basicConfig(level=logging.CRITICAL)
 
-assert "MEDLEYDB_PATH" in environ, """The environment variable MEDLEYDB_PATH
-is not set. Set the value of MEDLEYDB_PATH to your local path to MedeleyDB."""
-
-MEDLEYDB_PATH = environ["MEDLEYDB_PATH"]
-
-assert path.exists(MEDLEYDB_PATH), """The path: %s set for MEDLEYDB_PATH does
-not exist.""" % MEDLEYDB_PATH
+if "MEDLEYDB_PATH" not in environ:
+    print """Warning: The environment variable MEDLEYDB_PATH is not set.
+             As a result, any part of the code that requires the audio won't
+             work. If you don't need to access the audio, disregard this warning.
+             If you do, set the value of MEDLEYDB_PATH to your local path to
+             the top level Audio folder for MedeleyDB."""
+    MEDLEYDB_PATH = ""
+else:
+    MEDLEYDB_PATH = environ["MEDLEYDB_PATH"]
+    assert path.exists(MEDLEYDB_PATH), """The path: %s set for MEDLEYDB_PATH does
+    not exist.""" % MEDLEYDB_PATH
 
 INST_TAXONOMY = path.join(path.dirname(__file__), 'taxonomy.yaml')
+TRACK_LIST = path.join(path.dirname(__file__), 'tracklist_v1.txt')
+ANNOT_PATH = path.join(path.dirname(__file__), '../', 'Annotations')
+METADATA_PATH = path.join(path.dirname(__file__), '../', 'Metadata')
 
 from .utils import (
     load_melody_multitracks,
