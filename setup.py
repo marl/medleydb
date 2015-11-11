433323b081fd69d9a5b2d2804d699596e3ebb90d
Rachel Bittner <rachelbittner@spotify.com>
2015-11-11 11:28:11 -0500

    up version from 0.1.0 to 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     setup(
         name='medleydb',
 
-        version='0.1.0',
+        version='0.2.0',
 
         description='Python module for the MedleyDB dataset',
 
