2c93183b68ab0a5c1744c2ce7b5c708452a880ff
Rachel Bittner <rmb456@nyu.edu>
2016-01-13 18:41:14 -0500

    allow audio to be in a different location than the repo

--- medleydb/__init__.py
+++ medleydb/__init__.py
@@ -21,13 +21,25 @@ if "MEDLEYDB_PATH" not in environ:
     MEDLEYDB_PATH = ""
 else:
     MEDLEYDB_PATH = environ["MEDLEYDB_PATH"]
-    assert path.exists(MEDLEYDB_PATH), """The path: %s set for MEDLEYDB_PATH does
-    not exist.""" % MEDLEYDB_PATH
+    assert path.exists(MEDLEYDB_PATH), """The MEDLEYDB_PATH: %s does
+        not exist.""" % MEDLEYDB_PATH
 
+# The taxonomy, tracklist, annotations and metadata are version controlled and
+# stored inside the repository
 INST_TAXONOMY = path.join(path.dirname(__file__), 'taxonomy.yaml')
 TRACK_LIST = path.join(path.dirname(__file__), 'tracklist_v1.txt')
 ANNOT_PATH = path.join(path.dirname(__file__), '../', 'Annotations')
 METADATA_PATH = path.join(path.dirname(__file__), '../', 'Metadata')
+
+# Audio is downloaded separately and is not version controlled :'(.
+# This is the motivation for requiring the user to set the MEDLEYDB_PATH
+AUDIO_PATH = path.join(MEDLEYDB_PATH, 'Audio')
+if not path.exists(AUDIO_PATH):
+    AUDIO_PATH = None
+    print """Warning: The medleydb audio was not found at the expected path.
+             The module will still function, but without the
+             ability to access any of the audio."""
+    print """Expected MedleyDB audio path: %s""" % AUDIO_PATH
 
 from .utils import (
     load_melody_multitracks,
