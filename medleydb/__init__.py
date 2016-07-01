74c3bf7f245ce03c2ea0df53c73cb082c4484745
Carl Thomé <carlthome@gmail.com>
2016-07-01 12:05:14 +0200

    Updated resources path

--- medleydb/__init__.py
+++ medleydb/__init__.py
@@ -48,18 +48,23 @@ METADATA_PATH = path.join(path.dirname(__file__), '../', 'Metadata')
 
 TRACK_LIST = []
 with open(path.join(path.dirname(__file__),
+          'resources',
           'tracklist_v1.txt'), 'r') as fhandle:
     for line in fhandle.readlines():
         TRACK_LIST.append(line.strip('\n'))
 
-with open(path.join(path.dirname(__file__), 'taxonomy.yaml'), 'r') as f_handle:
+with open(path.join(path.dirname(__file__), 
+          'resources',
+          'taxonomy.yaml'), 'r') as f_handle:
     INST_TAXONOMY = yaml.load(f_handle)
 
 with open(path.join(path.dirname(__file__),
+          'resources',
           'instrument_f0_type.json'), 'r') as f_handle:
     INST_F0_TYPE = json.load(f_handle)
 
 with open(path.join(path.dirname(__file__),
+          'resources',
           'mixing_coefficients.yaml'), 'r') as fhandle:
     MIXING_COEFFICIENTS = yaml.load(fhandle)
 
