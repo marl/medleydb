96e5faced5c7701f2c50a8622b20c8b440bf939f
Carl Thomé <carlthome@gmail.com>
2016-07-04 10:51:29 +0200

    Added more filters to load_multitracks

--- medleydb/utils.py
+++ medleydb/utils.py
@@ -6,40 +6,62 @@
 from __future__ import print_function
 
 from . import multitrack as M
-from . import sox
-from . import TRACK_LIST
+from . import TRACK_LIST, sox
 
 
-def load_multitracks(track_list=None, melody_only=False):
+def load_multitracks(track_list=None,
+                     has_melody=None,
+                     is_excerpt=None,
+                     is_instrumental=None):
     """Load a list of multitracks.
 
     Examples:
-        # load all multitracks in MEDLEYDB_PATH.
+        # Load all multitracks in MEDLEYDB_PATH.
         >>> multitracks = load_multitracks()
 
-        # load a subset of the multitracks
+        # Load a subset of the multitracks
         >>> track_list = ['ArtistName1_TrackName1', \
                           'ArtistName2_TrackName2', \
                           'ArtistName3_TrackName3']
         >>> multitracks = load_multitracks(track_list)
 
-        # load all multitracks that have melody annotations.
-        >>> multitracks = load_multitracks(melody_only=True)
+        # Load all multitracks that have melody annotations.
+        >>> multitracks = load_multitracks(has_melody=True)
+
+        # Load all multitracks that have vocals but are not excerpts.
+        >>> multitracks = load_multitracks(is_instrumental=False, \
+                                           is_excerpt=False)
 
     Args:
         track_list (list): List of track ids in format 'Artist_Title'
-        melody_only (bool): Only select multitracks with melody annotations.
+        has_melody (Optional[bool]): Only select multitracks with melody
+                                 annotations. Set to `False` for only
+                                 multitracks without melody.
+        is_excerpt (Optional[bool]): Only select multitracks that are excerpts.
+                                  Set to `False` for only multitracks that are
+                                  not excerpts.
+        is_instrumental (Optional[bool]): Only select multitracks that are
+                                       instrumental. Set to `False` for only
+                                       multitracks with vocals.
 
     Returns:
         multitracks (dict): List of multitrack objects.
 
     """
-    if track_list == None:
+    if track_list is None:
         track_list = TRACK_LIST
     for track_id in track_list:
         track = M.MultiTrack(track_id)
-        if not melody_only or track.has_melody:
-            yield track
+        if has_melody is not None:
+            if has_melody != track.has_melody:
+                continue
+        if is_excerpt is not None:
+            if is_excerpt != track.is_excerpt:
+                continue
+        if is_instrumental is not None:
+            if is_instrumental != track.is_instrumental:
+                continue
+        yield track
 
 
 def get_files_for_instrument(instrument, multitrack_list=None):
@@ -118,12 +140,12 @@ def preview_audio(multitrack, selection='all', preview_length=8):
 
     if selection == 'all' or selection == 'stems':
         for track in stems:
-            print(("Previewing stem %r (%s)..."
-                  % (track.stem_idx, track.instrument)))
+            print(("Previewing stem %r (%s)..." % (track.stem_idx,
+                                                   track.instrument)))
             sox.quick_play(track.file_path, duration=preview_length)
 
     if selection == 'all' or selection == 'raw':
         for track in raw_audio:
-            print(("Previewing raw audio %r %r (%s)..."
-                  % (track.stem_idx, track.raw_idx, track.instrument)))
+            print(("Previewing raw audio %r %r (%s)..." % (
+                track.stem_idx, track.raw_idx, track.instrument)))
             sox.quick_play(track.file_path, duration=preview_length)
