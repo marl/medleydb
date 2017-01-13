5e58fab292d8a2d8110a5a2e2815c7276dd66a51
Rachel Bittner <rmb456@nyu.edu>
2017-01-12 21:27:18 -0500

    update to also remove release date

--- scripts/add_version_to_metadata.py
+++ scripts/add_version_to_metadata.py
@@ -9,20 +9,33 @@ import yaml
 from medleydb import METADATA_PATH
 
 
+def add_version(fpath):
+    with open(fpath, 'r') as fhandle:
+        data = yaml.load(fhandle)
+
+    if 'version' not in data.keys():
+        data['version'] = 1.2
+        with open(fpath, 'w') as fhandle:
+            yaml.dump(data, fhandle, indent=2, default_flow_style=False)
+
+
+def remove_release_date(fpath):
+    with open(fpath, 'r') as fhandle:
+        data = yaml.load(fhandle)
+
+    if 'release date' in data.keys():
+        data.pop('release date', None)
+        with open(fpath, 'w') as fhandle:
+            yaml.dump(data, fhandle, indent=2, default_flow_style=False)
+
+
 def main():
     """Loops over all metadata files and adds version information
     """
     metadata_files = glob.glob(os.path.join(METADATA_PATH, '*.yaml'))
     for fpath in metadata_files:
-        with open(fpath, 'r') as fhandle:
-            data = yaml.load(fhandle)
-
-        if 'version' not in data.keys():
-            data['version'] = 1.2
-            with open(fpath, 'w') as fhandle:
-                yaml.dump(data, fhandle, indent=2, default_flow_style=False)
-        else:
-            print("%s has version" % fpath)
+        add_version(fpath)
+        remove_release_date(fpath)
 
 
 if __name__ == "__main__":
